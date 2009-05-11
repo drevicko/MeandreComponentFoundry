b6fe23da1dc966d22b44551569005157911a0f93
xllora <xllora@dc7ab104-3778-4984-a5d9-166996b8e16d>
2009-05-11 05:25:39 +0000

    [FOUNDRY] Finxing a messed up component
    git-svn-id: svn://dev-svn.seasr.org/Components/Foundry/trunk@1087 dc7ab104-3778-4984-a5d9-166996b8e16d

--- src-tools/org/seasr/datatypes/tranformations/JavaStringToStrings.java
+++ src-tools/org/seasr/datatypes/tranformations/JavaStringToStrings.java
@@ -11,7 +11,9 @@ import org.meandre.annotations.Component.Licenses;
 import org.meandre.annotations.Component.Mode;
 import org.meandre.core.ComponentContext;
 import org.meandre.core.ComponentContextException;
+import org.meandre.core.ComponentContextProperties;
 import org.meandre.core.ComponentExecutionException;
+import org.meandre.core.ExecutableComponent;
 import org.meandre.core.system.components.ext.StreamDelimiter;
 import org.seasr.datatypes.BasicDataTypesTools;
 import org.seasr.meandre.components.tools.Names;
@@ -33,7 +35,7 @@ import org.seasr.meandre.components.tools.Names;
 		tags = "tools, tokenizer, counting, transformations",
 		description = "Converts a Java string into an equivalent string protocol buffer wrapper."
 )
-public class JavaStringToStrings {
+public class JavaStringToStrings implements ExecutableComponent  {
 	
 	//--------------------------------------------------------------------------------------------
 	
@@ -74,6 +76,18 @@ public class JavaStringToStrings {
 				throw new ComponentExecutionException(e);
 			}
 		}
+	}
+
+	public void dispose(ComponentContextProperties arg0)
+			throws ComponentExecutionException, ComponentContextException {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void initialize(ComponentContextProperties arg0)
+			throws ComponentExecutionException, ComponentContextException {
+		// TODO Auto-generated method stub
+		
 	}
 
 	//--------------------------------------------------------------------------------------------
