4bf2b36064215d8f6862ccc4e5ad8c6497138e7a
mikeh <mikeh@dc7ab104-3778-4984-a5d9-166996b8e16d>
2009-08-10 18:56:30 +0000

    new tuple logger component
    git-svn-id: svn://dev-svn.seasr.org/Components/Foundry/trunk@1363 dc7ab104-3778-4984-a5d9-166996b8e16d

---
+++ src-sentiment/org/seasr/meandre/components/sentiment/TupleLogger.java
@@ -0,0 +1,130 @@
+package org.seasr.meandre.components.sentiment;
+
+
+import java.util.List;
+import java.util.ArrayList;
+import java.util.Map;
+import java.util.HashMap;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.regex.Pattern;
+
+
+import org.meandre.annotations.Component;
+import org.meandre.annotations.ComponentInput;
+import org.meandre.annotations.ComponentOutput;
+import org.meandre.annotations.ComponentProperty;
+import org.meandre.annotations.Component.FiringPolicy;
+import org.meandre.annotations.Component.Licenses;
+import org.meandre.annotations.Component.Mode;
+import org.meandre.components.abstracts.AbstractExecutableComponent;
+import org.meandre.core.ComponentContext;
+import org.meandre.core.ComponentContextException;
+import org.meandre.core.ComponentContextProperties;
+import org.meandre.core.ComponentExecutionException;
+import org.seasr.datatypes.BasicDataTypes;
+import org.seasr.datatypes.BasicDataTypesTools;
+import org.seasr.datatypes.BasicDataTypes.Strings;
+import org.seasr.datatypes.BasicDataTypes.StringsMap;
+import org.seasr.meandre.components.tools.Names;
+import org.seasr.meandre.support.parsers.DataTypeParser;
+
+
+import org.seasr.meandre.support.tuples.DynamicTuple;
+import org.seasr.meandre.support.tuples.DynamicTuplePeer;
+
+
+
+/**
+ * This component perform POS tagging on the text passed using OpenNLP.
+ *
+ * @author Mike Haberman;
+ *
+ */
+
+//
+// General Path:   PosTagger -->TupleValueFrequencyCounter
+//
+
+
+@Component(
+		name = "tuple logger",
+		creator = "Mike Haberman",
+		baseURL = "meandre://seasr.org/components/tools/",
+		firingPolicy = FiringPolicy.all,
+		mode = Mode.compute,
+		rights = Licenses.UofINCSA,
+		tags = "tuple, tools, text, filter",
+		description = "This component prints the incoming set of tuples " ,
+		dependency = {"trove-2.0.3.jar","protobuf-java-2.0.3.jar"}
+)
+public class TupleLogger  extends AbstractExecutableComponent {
+	
+
+    //------------------------------ INPUTS ------------------------------------------------------
+	
+	@ComponentInput(
+			name = Names.PORT_TUPLES,
+			description = "set of tuples"
+	)
+	protected static final String IN_TUPLES = Names.PORT_TUPLES;
+	
+	@ComponentInput(
+			name = Names.PORT_META_TUPLE,
+			description = "meta data for tuples"
+	)
+	protected static final String IN_META_TUPLE = Names.PORT_META_TUPLE;
+	
+
+    //------------------------------ OUTPUTS -----------------------------------------------------
+	
+	@ComponentOutput(
+			name = Names.PORT_TUPLES,
+			description = "set of filtered tuples"
+	)
+	protected static final String OUT_TUPLES = Names.PORT_TUPLES;
+	
+	@ComponentOutput(
+			name = Names.PORT_META_TUPLE,
+			description = "meta data for the tuples (same as input)"
+	)
+	protected static final String OUT_META_TUPLE = Names.PORT_META_TUPLE;
+	
+	//----------------------------- PROPERTIES ---------------------------------------------------
+    //--------------------------------------------------------------------------------------------
+
+	
+	public void initializeCallBack(ComponentContextProperties ccp) throws Exception 
+	{	
+		
+	}
+
+	public void executeCallBack(ComponentContext cc) throws Exception 
+	{
+		
+		Strings inputMeta = (Strings) cc.getDataComponentFromInput(IN_META_TUPLE);
+		String[] meta = DataTypeParser.parseAsString(inputMeta);
+		String fields = meta[0];
+		DynamicTuplePeer inPeer = new DynamicTuplePeer(fields);
+		
+		
+		Strings input = (Strings) cc.getDataComponentFromInput(IN_TUPLES);
+		String[] tuples = DataTypeParser.parseAsString(input);
+		DynamicTuple tuple = inPeer.createTuple();
+		
+		List<String> output = new ArrayList<String>();
+		for (int i = 0; i < tuples.length; i++) {
+			tuple.setValues(tuples[i]);	
+			console.info(tuple.toString());
+		}
+				
+		
+		cc.pushDataComponentToOutput(OUT_TUPLES, input);
+		cc.pushDataComponentToOutput(OUT_META_TUPLE, inputMeta);
+		 
+	}
+
+    public void disposeCallBack(ComponentContextProperties ccp) throws Exception {
+        
+    }
+}
