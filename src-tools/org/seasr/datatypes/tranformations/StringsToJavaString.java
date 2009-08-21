a4a6e9b65510ed352a17f8f66091a99af305564c
capitanu <capitanu@dc7ab104-3778-4984-a5d9-166996b8e16d>
2009-08-21 16:06:30 +0000

    - added component to convert a google string to a java string
    git-svn-id: svn://dev-svn.seasr.org/Components/Foundry/trunk@1400 dc7ab104-3778-4984-a5d9-166996b8e16d

---
+++ src-tools/org/seasr/datatypes/tranformations/StringsToJavaString.java
@@ -0,0 +1,111 @@
+/**
+*
+* University of Illinois/NCSA
+* Open Source License
+*
+* Copyright (c) 2008, NCSA.  All rights reserved.
+*
+* Developed by:
+* The Automated Learning Group
+* University of Illinois at Urbana-Champaign
+* http://www.seasr.org
+*
+* Permission is hereby granted, free of charge, to any person obtaining
+* a copy of this software and associated documentation files (the
+* "Software"), to deal with the Software without restriction, including
+* without limitation the rights to use, copy, modify, merge, publish,
+* distribute, sublicense, and/or sell copies of the Software, and to
+* permit persons to whom the Software is furnished to do so, subject
+* to the following conditions:
+*
+* Redistributions of source code must retain the above copyright
+* notice, this list of conditions and the following disclaimers.
+*
+* Redistributions in binary form must reproduce the above copyright
+* notice, this list of conditions and the following disclaimers in
+* the documentation and/or other materials provided with the distribution.
+*
+* Neither the names of The Automated Learning Group, University of
+* Illinois at Urbana-Champaign, nor the names of its contributors may
+* be used to endorse or promote products derived from this Software
+* without specific prior written permission.
+*
+* THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+* EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+* MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+* IN NO EVENT SHALL THE CONTRIBUTORS OR COPYRIGHT HOLDERS BE LIABLE
+* FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF
+* CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
+* WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS WITH THE SOFTWARE.
+*
+*/
+
+package org.seasr.datatypes.tranformations;
+
+import org.meandre.annotations.Component;
+import org.meandre.annotations.ComponentInput;
+import org.meandre.annotations.ComponentOutput;
+import org.meandre.annotations.Component.FiringPolicy;
+import org.meandre.annotations.Component.Licenses;
+import org.meandre.annotations.Component.Mode;
+import org.meandre.components.abstracts.AbstractExecutableComponent;
+import org.meandre.core.ComponentContext;
+import org.meandre.core.ComponentContextProperties;
+import org.seasr.datatypes.BasicDataTypesTools;
+import org.seasr.datatypes.BasicDataTypes.Strings;
+import org.seasr.meandre.components.tools.Names;
+
+
+/**
+ * This component transforms a token count into a map.
+ *
+ * @author Boris Capitanu
+ *
+ */
+@Component(
+        name = "Strings To Java String",
+        creator = "Boris Capitanu",
+        baseURL = "meandre://seasr.org/components/tools/",
+        firingPolicy = FiringPolicy.all,
+        mode = Mode.compute,
+        rights = Licenses.UofINCSA,
+        tags = "tools, convert, transform",
+        description = "Converts a Google string into an equivalent Java string",
+        dependency = {"protobuf-java-2.0.3.jar"}
+)
+public class StringsToJavaString extends AbstractExecutableComponent {
+
+    //------------------------------ INPUTS ------------------------------------------------------
+
+    @ComponentInput(
+            name = Names.PORT_TEXT,
+            description = "The Google string to convert"
+    )
+    protected static final String IN_TEXT = Names.PORT_TEXT;
+
+    //------------------------------ OUTPUTS -----------------------------------------------------
+
+    @ComponentOutput(
+            name = Names.PORT_JAVA_STRING,
+            description = "The converted string"
+    )
+    protected static final String OUT_JAVA_STRING = Names.PORT_JAVA_STRING;
+
+
+    //--------------------------------------------------------------------------------------------
+
+    @Override
+    public void initializeCallBack(ComponentContextProperties ccp) throws Exception {
+    }
+
+    @Override
+    public void executeCallBack(ComponentContext cc) throws Exception {
+        cc.pushDataComponentToOutput(OUT_JAVA_STRING,
+                BasicDataTypesTools.stringsToStringArray(
+                        (Strings)cc.getDataComponentFromInput(IN_TEXT))[0]);
+    }
+
+    @Override
+    public void disposeCallBack(ComponentContextProperties ccp) throws Exception {
+    }
+}
