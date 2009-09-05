682114dd4574bee2f2f11671df03c01c1a37c043
capitanu <capitanu@dc7ab104-3778-4984-a5d9-166996b8e16d>
2009-09-05 19:17:58 +0000

    - added methods for computing MD5 checksums
    git-svn-id: svn://dev-svn.seasr.org/Components/Foundry/trunk@1434 dc7ab104-3778-4984-a5d9-166996b8e16d

--- src-support/org/seasr/meandre/support/crypto/Crypto.java
+++ src-support/org/seasr/meandre/support/crypto/Crypto.java
@@ -1,27 +1,35 @@
 
 package org.seasr.meandre.support.crypto;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
 import java.io.UnsupportedEncodingException;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
 
 
-public class Crypto {
+public abstract class Crypto {
 
-    public static String convertToHex(byte[] data) {
-        StringBuffer buf = new StringBuffer();
-        for (int i = 0; i < data.length; i++) {
-            int halfbyte = (data[i] >>> 4) & 0x0F;
-            int two_halfs = 0;
-            do {
-                if ((0 <= halfbyte) && (halfbyte <= 9))
-                    buf.append((char) ('0' + halfbyte));
-                else
-                    buf.append((char) ('a' + (halfbyte - 10)));
-                halfbyte = data[i] & 0x0F;
-            } while (two_halfs++ < 1);
+    static final byte[] HEX_CHAR_TABLE = {
+        (byte)'0', (byte)'1', (byte)'2', (byte)'3',
+        (byte)'4', (byte)'5', (byte)'6', (byte)'7',
+        (byte)'8', (byte)'9', (byte)'a', (byte)'b',
+        (byte)'c', (byte)'d', (byte)'e', (byte)'f'
+    };
+
+    public static String getHexString(byte[] raw) throws UnsupportedEncodingException
+    {
+        byte[] hex = new byte[2 * raw.length];
+        int index = 0;
+
+        for (byte b : raw) {
+            int v = b & 0xFF;
+            hex[index++] = HEX_CHAR_TABLE[v >>> 4];
+            hex[index++] = HEX_CHAR_TABLE[v & 0xF];
         }
-        return buf.toString();
+        return new String(hex, "ASCII");
     }
 
     public static String getSHA1Hash(String text) throws NoSuchAlgorithmException, UnsupportedEncodingException {
@@ -30,6 +38,62 @@ public class Crypto {
         md.update(text.getBytes("iso-8859-1"), 0, text.length());
         sha1hash = md.digest();
 
-        return convertToHex(sha1hash);
+        return getHexString(sha1hash);
     }
+
+    /**
+     * Creates an MD5 checksum for a file
+     *
+     * @param file The file
+     * @return The byte[] containing the MD5 checksum
+     * @throws IOException Thrown when an I/O error occurs
+     */
+    public static byte[] createMD5Checksum(File file) throws IOException {
+        MessageDigest complete;
+        InputStream fis = new FileInputStream(file);
+
+        try {
+            byte[] buffer = new byte[4096];
+
+            try {
+                complete = MessageDigest.getInstance("MD5");
+            }
+            catch (NoSuchAlgorithmException e) {
+                return null;
+            }
+
+            int numRead;
+            do {
+                if ((numRead = fis.read(buffer)) > 0) {
+                    complete.update(buffer, 0, numRead);
+                }
+            } while (numRead > 0);
+        }
+        finally {
+            fis.close();
+        }
+
+        return complete.digest();
+    }
+
+    /**
+     * Creates an MD5 checksum for a data block
+     * @param data The data
+     * @return The checksum
+     */
+    public static byte[] createMD5Checksum(byte[] data) {
+        MessageDigest complete;
+
+        try {
+            complete = MessageDigest.getInstance("MD5");
+        }
+        catch (NoSuchAlgorithmException e) {
+            return null;
+        }
+
+        complete.update(data);
+
+        return complete.digest();
+    }
+
 }
