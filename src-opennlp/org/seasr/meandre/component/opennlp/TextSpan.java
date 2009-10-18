9f7a26decc4b4f439a9c98a3f5657e73ba7ddff8
mikeh <mikeh@dc7ab104-3778-4984-a5d9-166996b8e16d>
2009-10-18 04:50:01 +0000

    added equals
    git-svn-id: svn://dev-svn.seasr.org/Components/Foundry/trunk@1555 dc7ab104-3778-4984-a5d9-166996b8e16d

--- src-opennlp/org/seasr/meandre/component/opennlp/TextSpan.java
+++ src-opennlp/org/seasr/meandre/component/opennlp/TextSpan.java
@@ -1,5 +1,58 @@
 package org.seasr.meandre.component.opennlp;
 
+
+
+/**
+*
+* University of Illinois/NCSA
+* Open Source License
+*
+* Copyright (c) 2008, NCSA.  All rights reserved.
+*
+* Developed by:
+* The Automated Learning Group
+* University of Illinois at Urbana-Champaign
+* http://www.seasr.org
+*
+* Permission is hereby granted, free of charge, to any person obtaining
+* a copy of this software and associated documentation files (the
+* "Software"), to deal with the Software without restriction, including
+* without limitation the rights to use, copy, modify, merge, publish,
+* distribute, sublicense, and/or sell copies of the Software, and to
+* permit persons to whom the Software is furnished to do so, subject
+* to the following conditions:
+*
+* Redistributions of source code must retain the above copyright
+* notice, this list of conditions and the following disclaimers.
+*
+* Redistributions in binary form must reproduce the above copyright
+* notice, this list of conditions and the following disclaimers in
+* the documentation and/or other materials provided with the distribution.
+*
+* Neither the names of The Automated Learning Group, University of
+* Illinois at Urbana-Champaign, nor the names of its contributors may
+* be used to endorse or promote products derived from this Software
+* without specific prior written permission.
+*
+* THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+* EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+* MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+* IN NO EVENT SHALL THE CONTRIBUTORS OR COPYRIGHT HOLDERS BE LIABLE
+* FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF
+* CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
+* WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS WITH THE SOFTWARE.
+*
+*/
+
+
+//
+//simple helper class to maintain text spans
+//this one keeps the actual text as well
+// this class should NOT be coupled tightly 
+// with any OPEN NLP stuff
+//
+
+
 public class TextSpan {
 	
 	int startIdx;
@@ -14,10 +67,13 @@ public class TextSpan {
 	public void reset()
 	{
 		startIdx = endIdx = 0;
+		this.span = null;
 	}
+	
 	public void setStart(int s) {
 		startIdx = s;
 	}
+	
 	public void setEnd(int e) {
 		endIdx = e;
 	}
@@ -28,8 +84,23 @@ public class TextSpan {
 		this.span = textSpan.replace("\n", " ").trim();
 	}
 	
-	public int getStart() {return startIdx;}
-	public int getEnd()   {return endIdx;}
-	public String getText() { return span;}
+	public boolean equals(TextSpan b) {
+		
+		if (b == null) return false;
+		
+		if (this.span == null &&
+			   b.span == null) return true;
+		
+		if (this.span == null) return false;
+		
+		return this.startIdx == b.startIdx &&
+		       this.endIdx   == b.endIdx   &&
+		       this.span.equals(b.span);
+		
+	}
+	
+	public int getStart()   {return startIdx;}
+	public int getEnd()     {return endIdx;}
+	public String getText() {return span;}
 
 }
