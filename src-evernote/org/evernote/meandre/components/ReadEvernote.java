2021be2776ec1fd0b69cc49c2665443da162d1a7
capitanu <capitanu@dc7ab104-3778-4984-a5d9-166996b8e16d>
2009-12-14 17:37:55 +0000

    [FOUNDRY] - reformatted
    git-svn-id: svn://dev-svn.seasr.org/Components/Foundry/trunk@1762 dc7ab104-3778-4984-a5d9-166996b8e16d

--- src-evernote/org/evernote/meandre/components/ReadEvernote.java
+++ src-evernote/org/evernote/meandre/components/ReadEvernote.java
@@ -46,149 +46,143 @@ import java.util.List;
 
 import org.apache.thrift.protocol.TBinaryProtocol;
 import org.apache.thrift.transport.THttpClient;
-import org.meandre.components.abstracts.AbstractExecutableComponent;
-
 import org.meandre.annotations.Component;
 import org.meandre.annotations.ComponentOutput;
 import org.meandre.annotations.ComponentProperty;
-
+import org.meandre.components.abstracts.AbstractExecutableComponent;
 import org.meandre.core.ComponentContext;
-import org.meandre.core.ComponentContextException;
 import org.meandre.core.ComponentContextProperties;
-import org.meandre.core.ComponentExecutionException;
 import org.seasr.datatypes.BasicDataTypesTools;
 import org.seasr.meandre.components.tools.Names;
 
-import com.evernote.edam.userstore.UserStore;
-import com.evernote.edam.userstore.AuthenticationResult;
-import com.evernote.edam.type.Notebook;
-import com.evernote.edam.type.User;
-import com.evernote.edam.type.Note;
-import com.evernote.edam.notestore.NoteStore;
 import com.evernote.edam.notestore.NoteFilter;
 import com.evernote.edam.notestore.NoteList;
+import com.evernote.edam.notestore.NoteStore;
+import com.evernote.edam.type.Note;
+import com.evernote.edam.type.Notebook;
+import com.evernote.edam.type.User;
+import com.evernote.edam.userstore.AuthenticationResult;
+import com.evernote.edam.userstore.UserStore;
 
-@Component(creator="Lily Dong",
-           description="Demomstrates how to write a interface " +
-           "to read all of the notes under owner's account at sandbox.evernote.com, " +
-           "and outputs note's title and note's content as XML text. " +
-           "It should be pointed out that sandbox.evernote.com is only a test-only server.",
-           name="Read Evernote",
-           tags="evernote, note, notebook, read",
-           baseURL="meandre://seasr.org/components/foundry/",
-           dependency = {"protobuf-java-2.2.0.jar"}
+@Component(
+        creator = "Lily Dong",
+        description = "Demomstrates how to write a interface " +
+            "to read all of the notes under owner's account at sandbox.evernote.com, " +
+            "and outputs note's title and note's content as XML text. " +
+            "It should be pointed out that sandbox.evernote.com is only a test-only server.",
+        name = "Read Evernote",
+        tags = "evernote, note, notebook, read",
+        baseURL = "meandre://seasr.org/components/foundry/",
+        dependency = {"protobuf-java-2.2.0.jar"}
 )
-
-public class ReadEvernote extends AbstractExecutableComponent
-{
-	//------------------------------ PROPERTIES --------------------------------------------------
-
-    @ComponentProperty(defaultValue="",
-                       description="This property sets the username.",
-                       name=Names.PROP_USERNAME)
-    final static String PROP_USERNAME = Names.PROP_USERNAME;
-
-    @ComponentProperty(defaultValue="",
-                       description="This property sets the password.",
-                       name=Names.PROP_PASSWORD)
-    final static String PROP_PASSWORD = Names.PROP_PASSWORD;
-
-    @ComponentProperty(defaultValue="seasr",
-                       description="This property sets the consumer key.",
-                       name=Names.PROP_CONSUMER_KEY)
-    final static String PROP_CONSUMER_KEY = Names.PROP_CONSUMER_KEY;
-
-    @ComponentProperty(defaultValue="d38423353add320b",
-                       description="This property sets the consumer secret.",
-                       name=Names.PROP_CONSUMER_SECRET)
-    final static String PROP_CONSUMER_SECERT = Names.PROP_CONSUMER_SECRET;
+public class ReadEvernote extends AbstractExecutableComponent {
 
     //------------------------------ OUTPUTS -----------------------------------------------------
 
-    @ComponentOutput(description="Outputs note's title.",
-                     name=Names.PORT_TITLE)
+    @ComponentOutput(
+            description = "Outputs note's title.",
+            name = Names.PORT_TITLE
+    )
     public final static String OUT_TITLE = Names.PORT_TITLE;
 
-    @ComponentOutput(description="Output note's content as XML text.",
-            		 name=Names.PORT_TEXT)
-    public final static String OUT_TEXT = Names.PORT_TEXT;
+    @ComponentOutput(
+            description = "Output note's content as XML text.",
+            name = Names.PORT_TEXT
+    )
+    public final static String OUT_XML = Names.PORT_TEXT;
 
-    static final String userStoreUrl = "https://sandbox.evernote.com/edam/user";
-    static final String noteStoreUrlBase = "http://sandbox.evernote.com/edam/note/";
+    //------------------------------ PROPERTIES --------------------------------------------------
 
-    /** When ready for execution.
-    *
-    * @param cc The component context
-    * @throws ComponentExecutionException An exception occurred during execution
-    * @throws ComponentContextException Illegal access to context
-    */
-    public void executeCallBack(ComponentContext cc)
-    throws Exception
-    {
+    @ComponentProperty(
+            defaultValue = "",
+            description = "This property sets the username.",
+            name = Names.PROP_USERNAME
+    )
+    protected static final String PROP_USERNAME = Names.PROP_USERNAME;
+
+    @ComponentProperty(
+            defaultValue = "",
+            description = "This property sets the password.",
+            name = Names.PROP_PASSWORD
+    )
+    protected static final String PROP_PASSWORD = Names.PROP_PASSWORD;
+
+    @ComponentProperty(
+            defaultValue = "seasr",
+            description = "This property sets the consumer key.",
+            name = Names.PROP_CONSUMER_KEY
+    )
+    protected static final String PROP_CONSUMER_KEY = Names.PROP_CONSUMER_KEY;
+
+    @ComponentProperty(
+            defaultValue = "d38423353add320b",
+            description = "This property sets the consumer secret.",
+            name = Names.PROP_CONSUMER_SECRET
+    )
+    protected static final String PROP_CONSUMER_SECERT = Names.PROP_CONSUMER_SECRET;
+
+    //--------------------------------------------------------------------------------------------
+
+
+    protected static final String userStoreUrl = "https://sandbox.evernote.com/edam/user";
+    protected static final String noteStoreUrlBase = "http://sandbox.evernote.com/edam/note/";
+
+
+    //--------------------------------------------------------------------------------------------
+
+    @Override
+    public void initializeCallBack(ComponentContextProperties ccp) throws Exception {
+    }
+
+    @Override
+    public void executeCallBack(ComponentContext cc) throws Exception {
     	String username = cc.getProperty(PROP_USERNAME),
          	   password = cc.getProperty(PROP_PASSWORD);
 
     	String consumerKey = cc.getProperty(PROP_CONSUMER_KEY),
   	      	   consumerSecret = cc.getProperty(PROP_CONSUMER_SECERT);
 
-        try {
-        	THttpClient userStoreTrans = new THttpClient(userStoreUrl);
-        	TBinaryProtocol userStoreProt = new TBinaryProtocol(userStoreTrans);
-        	UserStore.Client userStore = new UserStore.Client(
-        			userStoreProt, userStoreProt);
-        	boolean versionOk = userStore.checkVersion(
-        			"Evernote's Local Client API Usage",
-        			com.evernote.edam.userstore.Constants.EDAM_VERSION_MAJOR,
-        	        com.evernote.edam.userstore.Constants.EDAM_VERSION_MINOR);
-        	if (!versionOk) {
-        	      throw new Exception("Incomatible EDAM client protocol version");
-        	}
 
-        	AuthenticationResult authResult = userStore.authenticate(
-        			username, password, consumerKey, consumerSecret);
-        	User user = authResult.getUser();
-        	String authToken = authResult.getAuthenticationToken();
+    	THttpClient userStoreTrans = new THttpClient(userStoreUrl);
+    	TBinaryProtocol userStoreProt = new TBinaryProtocol(userStoreTrans);
+    	UserStore.Client userStore = new UserStore.Client(userStoreProt, userStoreProt);
 
-            String noteStoreUrl = noteStoreUrlBase + user.getShardId();
-            THttpClient noteStoreTrans = new THttpClient(noteStoreUrl);
-            TBinaryProtocol noteStoreProt = new TBinaryProtocol(noteStoreTrans);
-            NoteStore.Client noteStore = new NoteStore.Client(noteStoreProt,
-                noteStoreProt);
+    	boolean versionOk = userStore.checkVersion(
+    	        "Evernote's Local Client API Usage",
+    	        com.evernote.edam.userstore.Constants.EDAM_VERSION_MAJOR,
+    	        com.evernote.edam.userstore.Constants.EDAM_VERSION_MINOR);
+    	if (!versionOk) {
+    	    throw new Exception("Incomatible EDAM client protocol version");
+    	}
 
-            List<Notebook> notebooks =
-                (List<Notebook>)noteStore.listNotebooks(authToken);
-            for (Notebook notebook : notebooks) {
-            	console.info("Notebook: " + notebook.getName());
-                NoteFilter filter = new NoteFilter();
-                filter.setNotebookGuid(notebook.getGuid());
-                NoteList noteList = noteStore.findNotes(authToken, filter, 0, 100);
-                List<Note> notes = (List<Note>) noteList.getNotes();
-                for (Note note : notes) {
-                	cc.pushDataComponentToOutput(
-                			OUT_TITLE,
-                			BasicDataTypesTools.stringToStrings(note.getTitle()));
-                	cc.pushDataComponentToOutput(
-                			OUT_TEXT,
-                			BasicDataTypesTools.stringToStrings(noteStore.getNoteContent(authToken, note.getGuid())));
-                }
-            }
-        } catch(Exception e) {
-            throw new ComponentExecutionException(e);
-        }
+    	AuthenticationResult authResult = userStore.authenticate(
+    	        username, password, consumerKey, consumerSecret);
+    	User user = authResult.getUser();
+    	String authToken = authResult.getAuthenticationToken();
+
+    	String noteStoreUrl = noteStoreUrlBase + user.getShardId();
+    	THttpClient noteStoreTrans = new THttpClient(noteStoreUrl);
+    	TBinaryProtocol noteStoreProt = new TBinaryProtocol(noteStoreTrans);
+    	NoteStore.Client noteStore = new NoteStore.Client(noteStoreProt, noteStoreProt);
+
+    	List<Notebook> notebooks = noteStore.listNotebooks(authToken);
+
+    	for (Notebook notebook : notebooks) {
+    	    console.info("Notebook: " + notebook.getName());
+    	    NoteFilter filter = new NoteFilter();
+    	    filter.setNotebookGuid(notebook.getGuid());
+    	    NoteList noteList = noteStore.findNotes(authToken, filter, 0, 100);
+    	    List<Note> notes = noteList.getNotes();
+
+    	    for (Note note : notes) {
+    	        cc.pushDataComponentToOutput(OUT_TITLE, BasicDataTypesTools.stringToStrings(note.getTitle()));
+    	        cc.pushDataComponentToOutput(OUT_XML, BasicDataTypesTools.stringToStrings(noteStore.getNoteContent(authToken, note.getGuid())));
+    	    }
+    	}
     }
 
-    /**
-     * Call at the end of an execution flow.
-     */
-    public void initializeCallBack(ComponentContextProperties ccp)
-    throws Exception {
-    }
-
-    /**
-     * Called when a flow is started.
-     */
-    public void disposeCallBack(ComponentContextProperties ccp)
-    throws Exception {
+    @Override
+    public void disposeCallBack(ComponentContextProperties ccp) throws Exception {
     }
 }
 
