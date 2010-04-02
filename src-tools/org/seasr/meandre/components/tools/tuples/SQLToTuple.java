5dbbbd3852471b88db74eb34b8e5dd20caad76e1
mikeh <mikeh@dc7ab104-3778-4984-a5d9-166996b8e16d>
2010-04-02 20:45:29 +0000

    initial import
    git-svn-id: svn://dev-svn.seasr.org/Components/Foundry/trunk@1936 dc7ab104-3778-4984-a5d9-166996b8e16d

---
+++ src-tools/org/seasr/meandre/components/tools/tuples/SQLToTuple.java
@@ -0,0 +1,240 @@
+/**
+*
+* University of Illinois/NCSA
+* Open Source License
+*
+* Copyright (c) 2008, NCSA.  All rights reserved.
+*
+* Developed by:
+* The Automated Learning Group
+* University of Illinois at Urbana-Champaign
+* http://www.seasr.org
+*
+* Permission is hereby granted, free of charge, to any person obtaining
+* a copy of this software and associated documentation files (the
+* "Software"), to deal with the Software without restriction, including
+* without limitation the rights to use, copy, modify, merge, publish,
+* distribute, sublicense, and/or sell copies of the Software, and to
+* permit persons to whom the Software is furnished to do so, subject
+* to the following conditions:
+*
+* Redistributions of source code must retain the above copyright
+* notice, this list of conditions and the following disclaimers.
+*
+* Redistributions in binary form must reproduce the above copyright
+* notice, this list of conditions and the following disclaimers in
+* the documentation and/or other materials provided with the distribution.
+*
+* Neither the names of The Automated Learning Group, University of
+* Illinois at Urbana-Champaign, nor the names of its contributors may
+* be used to endorse or promote products derived from this Software
+* without specific prior written permission.
+*
+* THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+* EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+* MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+* IN NO EVENT SHALL THE CONTRIBUTORS OR COPYRIGHT HOLDERS BE LIABLE
+* FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF
+* CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
+* WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS WITH THE SOFTWARE.
+*
+*/
+
+package org.seasr.meandre.components.tools.tuples;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.InputStreamReader;
+import java.net.URL;
+
+import org.meandre.annotations.Component;
+import org.meandre.annotations.ComponentOutput;
+import org.meandre.annotations.ComponentProperty;
+import org.meandre.annotations.Component.FiringPolicy;
+import org.meandre.annotations.Component.Licenses;
+import org.meandre.annotations.Component.Mode;
+import org.meandre.components.abstracts.AbstractExecutableComponent;
+import org.meandre.core.ComponentContext;
+import org.meandre.core.ComponentContextProperties;
+import org.seasr.datatypes.BasicDataTypesTools;
+import org.seasr.datatypes.BasicDataTypes.Strings;
+import org.seasr.datatypes.BasicDataTypes.StringsArray;
+import org.seasr.meandre.components.tools.Names;
+import org.seasr.meandre.support.components.tuples.SimpleTuple;
+import org.seasr.meandre.support.components.tuples.SimpleTuplePeer;
+
+
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+import java.sql.ResultSetMetaData;
+
+
+
+
+/**
+ * This component reads from an sql SELECT pushes its content inside of a tuple
+ *
+ * @author Mike Haberman
+ *
+ */
+
+@Component(
+		name = "SQL To Tuple",
+		creator = "Mike Haberman",
+		baseURL = "meandre://seasr.org/components/foundry/",
+		firingPolicy = FiringPolicy.all,
+		mode = Mode.compute,
+		rights = Licenses.UofINCSA,
+		tags = "tuple, tools, dbase",
+		description = "This component reads a mysql database",
+		dependency = {"trove-2.0.3.jar","protobuf-java-2.2.0.jar"}
+)
+public class SQLToTuple extends AbstractExecutableComponent {
+
+	//------------------------------ OUTPUTS -----------------------------------------------------
+
+	@ComponentOutput(
+			name = Names.PORT_TUPLES,
+			description = "tuples (based on the SQL)" +
+			    "<br>TYPE: org.seasr.datatypes.BasicDataTypes.StringsArray"
+	)
+	protected static final String OUT_TUPLES = Names.PORT_TUPLES;
+
+	@ComponentOutput(
+			name = Names.PORT_META_TUPLE,
+			description = "meta data for the tuple (column names from select!)" +
+                "<br>TYPE: org.seasr.datatypes.BasicDataTypes.Strings"
+	)
+	protected static final String OUT_META_TUPLE = Names.PORT_META_TUPLE;
+
+
+	//----------------------------- PROPERTIES ---------------------------------------------------
+
+	@ComponentProperty(
+			name = "user",
+			description = "user",
+		    defaultValue = ""
+	)
+	protected static final String PROP_USER = "user";
+	
+	@ComponentProperty(
+			name = "password",
+			description = "password",
+		    defaultValue = ""
+	)
+	protected static final String PROP_PASSWORD = "password";
+	
+	@ComponentProperty(
+			name = "hostURL",
+			description = "protocol://host/database: eg. jdbc:mysql://localhost/testDB",
+		    defaultValue = ""
+	)
+	protected static final String PROP_URL = "hostURL";
+	
+	@ComponentProperty(
+			name = "select",
+			description = "e.g. select * from a,b where a.id = b.id",
+		    defaultValue = ""
+	)
+	protected static final String PROP_SELECT = "select";
+	
+	
+	@ComponentProperty(
+			name = "JDBCDriver",
+			description = "jdbc driver to be loaded, jar must be in classpath",
+		    defaultValue = "com.mysql.jdbc.Driver"
+	)
+	protected static final String PROP_JDBC = "JDBCDriver";
+
+
+
+	//--------------------------------------------------------------------------------------------
+	SimpleTuplePeer outPeer;
+	SimpleTuple outTuple;
+
+	//--------------------------------------------------------------------------------------------
+
+
+	Connection connect = null;
+	String SQL;
+	
+	@Override
+    public void initializeCallBack(ComponentContextProperties ccp) throws Exception 
+    {
+		String user        = ccp.getProperty(PROP_USER).trim();
+	    String password    = ccp.getProperty(PROP_PASSWORD).trim();
+	    String hostURL     = ccp.getProperty(PROP_URL).trim();
+	    String JDBC_DRIVER = ccp.getProperty(PROP_JDBC).trim();
+	    
+	    SQL    = ccp.getProperty(PROP_SELECT).trim();
+	    
+		// This will load the MySQL driver, each DB has its own driver
+		Class.forName(JDBC_DRIVER);
+		
+		// Setup the connection with the DB
+		connect = DriverManager.getConnection(hostURL + "?" + "user="+user + "&password="+password);
+	}
+
+	@Override
+    public void executeCallBack(ComponentContext cc) throws Exception 
+    {
+		
+		Statement statement = null;
+		ResultSet resultSet = null;
+		
+		// Statements allow to issue SQL queries to the database
+		statement = connect.createStatement();
+		// Result set get the result of the SQL query
+		resultSet = statement.executeQuery(SQL);
+		ResultSetMetaData rsMetaData = resultSet.getMetaData();
+
+	    int numberOfColumns = rsMetaData.getColumnCount();
+	    String[] fieldNames = new String[numberOfColumns];
+	    for (int i = 0; i < numberOfColumns; i++) {
+		      String columnName = rsMetaData.getColumnName(i+1);
+		      fieldNames[i] = columnName;
+	    }
+	    outPeer = new SimpleTuplePeer(fieldNames);
+		outTuple = outPeer.createTuple();
+		
+		List<Strings> output = new ArrayList<Strings>();
+		while (resultSet.next()) {
+			for (int i = 0; i < numberOfColumns; i++) {
+			      String columnName = rsMetaData.getColumnName(i+1);
+			      String value = resultSet.getString(columnName);
+			      outTuple.setValue(i, value);
+			}
+			output.add(outTuple.convert());
+		}
+		
+		
+		resultSet.close();
+		statement.close();
+		
+		
+		Strings[] results = new Strings[output.size()];
+		output.toArray(results);
+		StringsArray outputSafe = BasicDataTypesTools.javaArrayToStringsArray(results);
+		cc.pushDataComponentToOutput(OUT_TUPLES, outputSafe);
+
+	    //
+		// metaData for this tuple producer
+		//
+	    cc.pushDataComponentToOutput(OUT_META_TUPLE, outPeer.convert());
+	    
+	}
+
+    @Override
+    public void disposeCallBack(ComponentContextProperties ccp) throws Exception {
+    	
+    	connect.close();
+    }
+}
