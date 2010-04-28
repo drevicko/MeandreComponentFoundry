87ee95321331c375634ade734bdd990fae726bbf
lauvil <lauvil@dc7ab104-3778-4984-a5d9-166996b8e16d>
2010-04-28 02:20:37 +0000

    updated description and tags... minor formatting
    git-svn-id: svn://dev-svn.seasr.org/Components/Foundry/trunk@1967 dc7ab104-3778-4984-a5d9-166996b8e16d

--- src-tools/org/seasr/meandre/components/tools/tuples/GoogleSearchToTuple.java
+++ src-tools/org/seasr/meandre/components/tools/tuples/GoogleSearchToTuple.java
@@ -42,12 +42,12 @@
 
 package org.seasr.meandre.components.tools.tuples;
 
-
 import java.net.URLEncoder;
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-import java.util.HashMap;
 
 import org.json.JSONArray;
 import org.json.JSONObject;
@@ -65,14 +65,11 @@ import org.seasr.datatypes.BasicDataTypes.Strings;
 import org.seasr.datatypes.BasicDataTypes.StringsArray;
 import org.seasr.meandre.components.abstracts.AbstractExecutableComponent;
 import org.seasr.meandre.components.tools.Names;
-
-
 import org.seasr.meandre.support.components.datatype.parsers.DataTypeParser;
 import org.seasr.meandre.support.components.tuples.SimpleTuple;
 import org.seasr.meandre.support.components.tuples.SimpleTuplePeer;
 import org.seasr.meandre.support.components.tuples.TupleUtilities;
 import org.seasr.meandre.support.generic.io.HttpUtils;
-
 
 /**
  * This component uses the google ajas/REST api for doing searches (web only)
@@ -88,25 +85,26 @@ import org.seasr.meandre.support.generic.io.HttpUtils;
 		firingPolicy = FiringPolicy.all,
 		mode = Mode.compute,
 		rights = Licenses.UofINCSA,
-		tags = "tuple, tools, text, filter",
-		description = "This component does a google search using the google search api ",
+		tags = "tuple, google, search, text",
+		description = "This component performs a google search by accessing the google search api."+
+		"The search term is an input and the search results are output as tuples.",
 		dependency = {"trove-2.0.3.jar","protobuf-java-2.2.0.jar"}
 )
-public class GoogleSearchToTuple extends AbstractExecutableComponent 
+public class GoogleSearchToTuple extends AbstractExecutableComponent
 {
 
 	//------------------------------ INPUTS -----------------------------------------------------
 	@ComponentInput(
 			name = "query",
-			description = "what to query for"
+			description = "search query"
 	)
 	protected static final String IN_QUERY = "query";
-	
+
 	//------------------------------ OUTPUTS -----------------------------------------------------
 
 	@ComponentOutput(
 			name = Names.PORT_TUPLES,
-			description = "tuples (set based on search results)" +
+			description = "search results represented as tuples" +
 			    "<br>TYPE: org.seasr.datatypes.BasicDataTypes.StringsArray"
 	)
 	protected static final String OUT_TUPLES = Names.PORT_TUPLES;
@@ -117,22 +115,12 @@ public class GoogleSearchToTuple extends AbstractExecutableComponent
                 "<br>TYPE: org.seasr.datatypes.BasicDataTypes.Strings"
 	)
 	protected static final String OUT_META_TUPLE = Names.PORT_META_TUPLE;
-	
+
 	//----------------------------- PROPERTIES ---------------------------------------------------
 
-	/*
-	@ComponentProperty(
-			name = "query",
-			description = "what to query for",
-		    defaultValue = ""
-	)
-	protected static final String PROP_QUERY = "query";
-	*/
-	
-	
 	@ComponentProperty(
 			name = "count",
-			description = "how many results to push to the output",
+			description = "number of search results to output",
 		    defaultValue = "25"
 	)
 	protected static final String PROP_COUNT = "count";
@@ -140,65 +128,58 @@ public class GoogleSearchToTuple extends AbstractExecutableComponent
 	//--------------------------------------------------------------------------------------------
 
 	SimpleTuplePeer tuplePeer;
-
-	//--------------------------------------------------------------------------------------------
     Map<String,String> columnMap = new HashMap<String,String>();
     int count;
-    
-    
+
+    //--------------------------------------------------------------------------------------------
+
 	@Override
-    public void initializeCallBack(ComponentContextProperties ccp) throws Exception 
+    public void initializeCallBack(ComponentContextProperties ccp) throws Exception
     {
 		count = Integer.parseInt(ccp.getProperty(PROP_COUNT).trim());
-		/*
-	    query = ccp.getProperty(PROP_QUERY).trim();
-	    query = URLEncoder.encode(query, "UTF-8");
-	    */    
 	    String fields[] = {"url","title", "content"};
 		tuplePeer = new SimpleTuplePeer(fields);
-		
+
 		columnMap.put("titleNoFormatting", "title");
-		
 	}
-	
+
 	int start = 0;
-	protected String buildURL(String query) 
+	protected String buildURL(String query)
 	{
 		StringBuilder location = new StringBuilder();
 	    location.append("http://ajax.googleapis.com/ajax/services/search/web?v=1.0");
 	    location.append("&start=").append(start); // TODO, add a paging property ?
 	    location.append("&rsz=large");            // small == 4 results, large == 8 results
 	    location.append("&q=").append(query);
-	    	
+
 	    String url = location.toString();
 	    start += 8; // 8 is what large
-	    
+
 	    return url;
 	}
 
 	@Override
-    public void executeCallBack(ComponentContext cc) throws Exception 
+    public void executeCallBack(ComponentContext cc) throws Exception
     {
 		String[] input = DataTypeParser.parseAsString(cc.getDataComponentFromInput(IN_QUERY));
 		String query = input[0];
 		query = URLEncoder.encode(query, "UTF-8");
-		
-		
+
 		List<Strings> output = new ArrayList<Strings>();
-		
+
 		while (output.size() < count) {
 			String url = buildURL(query);
 			console.info("reading location " + url);
 			String json = HttpUtils.doGET(url, null);
 
-			List<SimpleTuple> tuples = jsonToTuples(json, tuplePeer, columnMap);	 
-			
+			List<SimpleTuple> tuples = jsonToTuples(json, tuplePeer, columnMap);
+
 			for (int i = 0; i < tuples.size(); i++) {
 				SimpleTuple tuple = tuples.get(i);
 				output.add(tuple.convert());
 			}
 		}
-		
+
 		Strings[] results = new Strings[output.size()];
 		output.toArray(results);
 		StringsArray outputSafe = BasicDataTypesTools.javaArrayToStringsArray(results);
@@ -208,47 +189,64 @@ public class GoogleSearchToTuple extends AbstractExecutableComponent
 		// metaData for this tuple producer
 		//
 	    cc.pushDataComponentToOutput(OUT_META_TUPLE, tuplePeer.convert());
-
 	}
 
     @Override
     public void disposeCallBack(ComponentContextProperties ccp) throws Exception {
     }
-    
-    public List<SimpleTuple> jsonToTuples(String jsonData, 
+
+    public List<SimpleTuple> jsonToTuples(String jsonData,
     		                              SimpleTuplePeer peer,
-    		                              Map<String,String> columnMap) 
+    		                              Map<String,String> columnMap)
     throws Exception
     {
     	JSONObject json   = new JSONObject(jsonData);
     	JSONObject res    = json.getJSONObject("responseData");
     	JSONArray results = res.getJSONArray("results");
     	ArrayList<SimpleTuple> out = new ArrayList<SimpleTuple>();
-    	
-    	
+
     	int urlIdx     = TupleUtilities.getFieldIndexFromName(peer, "url",               columnMap);
     	int titleIdx   = TupleUtilities.getFieldIndexFromName(peer, "titleNoFormatting", columnMap);
     	int contentIdx = TupleUtilities.getFieldIndexFromName(peer, "content",           columnMap);
-    	
-    	
+
     	int size = results.length();
 
 		for (int i = 0; i < size;i++) {
-			
+
 			SimpleTuple tuple = peer.createTuple();
 			JSONObject fields = results.getJSONObject(i);
-			
+
 			String url      = fields.getString("url");
             String title    = fields.getString("titleNoFormatting");
             String content  = fields.getString("content");
-            
+
             tuple.setValue(urlIdx,     url);
             tuple.setValue(titleIdx,   title);
             tuple.setValue(contentIdx, content);
-            
-            out.add(tuple); 
+
+            out.add(tuple);
 		}
 
-    	return out;	
+    	return out;
+    }
+
+    //--------------------------------------------------------------------------------------------
+
+    @Override
+    protected void handleStreamInitiators() throws Exception {
+        if (!inputPortsWithInitiators.containsAll(Arrays.asList(new String[] { IN_QUERY })))
+            console.severe("Unbalanced stream delimiter received - the delimiters should arrive on all ports at the same time when FiringPolicy = ALL");
+
+        componentContext.pushDataComponentToOutput(OUT_META_TUPLE, componentContext.getDataComponentFromInput(IN_QUERY));
+        componentContext.pushDataComponentToOutput(OUT_TUPLES, componentContext.getDataComponentFromInput(IN_QUERY));
+    }
+
+    @Override
+    protected void handleStreamTerminators() throws Exception {
+        if (!inputPortsWithTerminators.containsAll(Arrays.asList(new String[] { IN_QUERY })))
+            console.severe("Unbalanced stream delimiter received - the delimiters should arrive on all ports at the same time when FiringPolicy = ALL");
+
+        componentContext.pushDataComponentToOutput(OUT_META_TUPLE, componentContext.getDataComponentFromInput(IN_QUERY));
+        componentContext.pushDataComponentToOutput(OUT_TUPLES, componentContext.getDataComponentFromInput(IN_QUERY));
     }
 }
