fe715acf7f6df5393a2f7f02c70ad2917ec19a21
lilydong <lilydong@dc7ab104-3778-4984-a5d9-166996b8e16d>
2010-05-11 15:32:25 +0000

    small change
    git-svn-id: svn://dev-svn.seasr.org/Components/Foundry/trunk@2009 dc7ab104-3778-4984-a5d9-166996b8e16d

--- src-tools/org/seasr/meandre/components/tools/text/io/UploadFile.java
+++ src-tools/org/seasr/meandre/components/tools/text/io/UploadFile.java
@@ -92,7 +92,7 @@ import com.oreilly.servlet.multipart.Part;
 
 @Component(
         creator = "Lily Dong",
-        description = "Uploads and submits text files from user's own machine. " +
+        description = "Uploads and submits files from user's own machine. " +
         "This component works with Fluid Infusion 0.8.",
         name = "Upload File",
         tags = "file input upload text",
@@ -167,7 +167,7 @@ public class UploadFile extends GenericTemplate {
 	    maxFileSize = Integer.parseInt(ccp.getProperty(PROP_MAX_SIZE));
 	    maxFileSize *= 1024 * 1024;
 
-	    context.put("title", "File Upload");
+	    context.put("title", "File Uploader");
 
 	    context.put("FPath", "/public/resources/fluid/fluid-components");
 	    String sFluidDir = ccp.getPublicResourcesDirectory() + File.separator + "fluid";
