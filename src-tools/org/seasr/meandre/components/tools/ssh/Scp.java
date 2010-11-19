07f354c338c571325e4f7891e37a67132a3e881c
capitanu <capitanu@dc7ab104-3778-4984-a5d9-166996b8e16d>
2010-11-19 16:57:08 +0000

    - updated descriptions
    git-svn-id: svn://dev-svn.seasr.org/Components/Foundry/trunk@2341 dc7ab104-3778-4984-a5d9-166996b8e16d

--- src-tools/org/seasr/meandre/components/tools/ssh/Scp.java
+++ src-tools/org/seasr/meandre/components/tools/ssh/Scp.java
@@ -65,8 +65,8 @@ import com.sshtools.j2ssh.transport.IgnoreHostKeyVerification;
         creator = "Boris Capitanu",
         description = "Securely copies a file or directory from a local to a remote server " +
         		"or from a remote server to local. One of the source or destination inputs needs to " +
-        		"specify the remote resource to be copied in the following format:  [host]:[path]\n" +
-        		"Example: source=my.server.com:/tmp/file.txt   destination=/tmp\n",
+        		"specify the remote resource to be copied in the following format:  [host]:[path]<br>" +
+        		"Example: source=my.server.com:/tmp/file.txt   destination=/tmp",
         name = "SCP",
         tags = "scp, file",
         rights = Licenses.UofINCSA,
@@ -78,32 +78,57 @@ public class Scp extends AbstractExecutableComponent {
     //------------------------------ INPUTS ------------------------------------------------------
 
     @ComponentInput(
-            description = "The user name to authenticate to the remote server",
-            name = "username"
+            name = "username",
+            description = "The user name to authenticate to the remote server" +
+                          "<br>TYPE: java.lang.String" +
+                          "<br>TYPE: org.seasr.datatypes.BasicDataTypes.Strings" +
+                          "<br>TYPE: byte[]" +
+                          "<br>TYPE: org.seasr.datatypes.BasicDataTypes.Bytes" +
+                          "<br>TYPE: java.lang.Object"
     )
     protected static final String IN_USERNAME = "username";
 
     @ComponentInput(
-            description = "The password",
-            name = "password"
+            name = "password",
+            description = "The password" +
+                          "<br>TYPE: java.lang.String" +
+                          "<br>TYPE: org.seasr.datatypes.BasicDataTypes.Strings" +
+                          "<br>TYPE: byte[]" +
+                          "<br>TYPE: org.seasr.datatypes.BasicDataTypes.Bytes" +
+                          "<br>TYPE: java.lang.Object"
     )
     protected static final String IN_PASSWORD = "password";
     
     @ComponentInput(
-            description = "The source file or directory",
-            name = "source"
+            name = "source",
+            description = "The source file or directory" +
+                          "<br>TYPE: java.lang.String" +
+                          "<br>TYPE: org.seasr.datatypes.BasicDataTypes.Strings" +
+                          "<br>TYPE: byte[]" +
+                          "<br>TYPE: org.seasr.datatypes.BasicDataTypes.Bytes" +
+                          "<br>TYPE: java.lang.Object"
     )
     protected static final String IN_SOURCE = "source";
 
     @ComponentInput(
-            description = "The destination file or directory",
-            name = "destination"
+            name = "destination",
+            description = "The destination file or directory" +
+                          "<br>TYPE: java.lang.String" +
+                          "<br>TYPE: org.seasr.datatypes.BasicDataTypes.Strings" +
+                          "<br>TYPE: byte[]" +
+                          "<br>TYPE: org.seasr.datatypes.BasicDataTypes.Bytes" +
+                          "<br>TYPE: java.lang.Object"
     )
     protected static final String IN_DESTINATION = "destination";
     
     @ComponentInput(
-            description = "Recursive?",
-            name = "recursive"
+            name = "recursive",
+            description = "Recursive?" +
+                          "<br>TYPE: java.lang.String" +
+                          "<br>TYPE: org.seasr.datatypes.BasicDataTypes.Strings" +
+                          "<br>TYPE: byte[]" +
+                          "<br>TYPE: org.seasr.datatypes.BasicDataTypes.Bytes" +
+                          "<br>TYPE: java.lang.Object"
     )
     protected static final String IN_RECURSIVE = "recursive";
 
