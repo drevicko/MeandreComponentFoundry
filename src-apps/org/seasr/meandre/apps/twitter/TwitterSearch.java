42254b647f8a6a56e6ed4835deb07c8e4ecf4d35
capitanu <capitanu@dc7ab104-3778-4984-a5d9-166996b8e16d>
2011-02-26 02:37:49 +0000

    - added two components
    git-svn-id: svn://dev-svn.seasr.org/Components/Foundry/trunk@2433 dc7ab104-3778-4984-a5d9-166996b8e16d

---
+++ src-apps/org/seasr/meandre/apps/twitter/TwitterSearch.java
@@ -0,0 +1,176 @@
+/**
+ *
+ * University of Illinois/NCSA
+ * Open Source License
+ *
+ * Copyright (c) 2008, NCSA.  All rights reserved.
+ *
+ * Developed by:
+ * The Automated Learning Group
+ * University of Illinois at Urbana-Champaign
+ * http://www.seasr.org
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining
+ * a copy of this software and associated documentation files (the
+ * "Software"), to deal with the Software without restriction, including
+ * without limitation the rights to use, copy, modify, merge, publish,
+ * distribute, sublicense, and/or sell copies of the Software, and to
+ * permit persons to whom the Software is furnished to do so, subject
+ * to the following conditions:
+ *
+ * Redistributions of source code must retain the above copyright
+ * notice, this list of conditions and the following disclaimers.
+ *
+ * Redistributions in binary form must reproduce the above copyright
+ * notice, this list of conditions and the following disclaimers in
+ * the documentation and/or other materials provided with the distribution.
+ *
+ * Neither the names of The Automated Learning Group, University of
+ * Illinois at Urbana-Champaign, nor the names of its contributors may
+ * be used to endorse or promote products derived from this Software
+ * without specific prior written permission.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+ * EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+ * MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+ * IN NO EVENT SHALL THE CONTRIBUTORS OR COPYRIGHT HOLDERS BE LIABLE
+ * FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF
+ * CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
+ * WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS WITH THE SOFTWARE.
+ *
+ */
+
+package org.seasr.meandre.apps.twitter;
+
+import java.io.InputStreamReader;
+import java.net.URL;
+import java.net.URLEncoder;
+import java.util.Arrays;
+
+import org.apache.commons.lang.StringEscapeUtils;
+import org.json.JSONArray;
+import org.json.JSONObject;
+import org.meandre.annotations.Component;
+import org.meandre.annotations.Component.FiringPolicy;
+import org.meandre.annotations.Component.Licenses;
+import org.meandre.annotations.Component.Mode;
+import org.meandre.annotations.ComponentInput;
+import org.meandre.annotations.ComponentOutput;
+import org.meandre.core.ComponentContext;
+import org.meandre.core.ComponentContextProperties;
+import org.seasr.datatypes.core.BasicDataTypes.StringsArray;
+import org.seasr.datatypes.core.DataTypeParser;
+import org.seasr.datatypes.core.Names;
+import org.seasr.meandre.components.abstracts.AbstractExecutableComponent;
+import org.seasr.meandre.support.components.tuples.SimpleTuple;
+import org.seasr.meandre.support.components.tuples.SimpleTuplePeer;
+import org.seasr.meandre.support.generic.io.IOUtils;
+
+/**
+ * @author Boris Capitanu
+ */
+
+@Component(
+        name = "Twitter Search",
+        creator = "Boris Capitanu",
+        baseURL = "meandre://seasr.org/components/foundry/",
+        firingPolicy = FiringPolicy.all,
+        mode = Mode.compute,
+        rights = Licenses.UofINCSA,
+        tags = "twitter, search",
+        description = "This component searches Twitter for stuff" ,
+        dependency = {"protobuf-java-2.2.0.jar"}
+)
+public class TwitterSearch extends AbstractExecutableComponent {
+
+    //------------------------------ INPUTS ------------------------------------------------------
+
+    @ComponentInput(
+            name = Names.PORT_TEXT,
+            description = "The query string" +
+                "<br>TYPE: org.seasr.datatypes.BasicDataTypes.Strings"
+    )
+    protected static final String IN_TEXT = Names.PORT_TEXT;
+
+    //------------------------------ OUTPUTS -----------------------------------------------------
+
+    @ComponentOutput(
+            name = Names.PORT_TUPLES,
+            description = "The search results" +
+                "<br>TYPE: org.seasr.datatypes.BasicDataTypes.Strings"
+    )
+    protected static final String OUT_TUPLES = Names.PORT_TUPLES;
+
+    @ComponentOutput(
+            name = Names.PORT_META_TUPLE,
+            description = "The meta data for the tuples" +
+                "<br>TYPE: org.seasr.datatypes.BasicDataTypes.Strings"
+    )
+    protected static final String OUT_META_TUPLE = Names.PORT_META_TUPLE;
+
+    //--------------------------------------------------------------------------------------------
+
+
+    protected static final String TWITTER_SEARCH_API_URL = "http://search.twitter.com/search.json?q=%s&rpp=100";
+
+    //--------------------------------------------------------------------------------------------
+
+    @Override
+    public void initializeCallBack(ComponentContextProperties ccp) throws Exception {
+    }
+
+    @Override
+    public void executeCallBack(ComponentContext cc) throws Exception {
+        String query = DataTypeParser.parseAsString(cc.getDataComponentFromInput(IN_TEXT))[0];
+        URL twitterSearchUrl = new URL(String.format(TWITTER_SEARCH_API_URL, URLEncoder.encode(query, "UTF-8")));
+        String response = IOUtils.getTextFromReader(new InputStreamReader(twitterSearchUrl.openStream()));
+
+        SimpleTuplePeer outPeer = new SimpleTuplePeer(new String[] { "from_user", "from_user_id_str", "profile_image_url", "created_at", "text" });
+        StringsArray.Builder tweetsBuilder = StringsArray.newBuilder();
+
+        JSONObject joResponse = new JSONObject(response);
+        JSONArray jaResults = joResponse.getJSONArray("results");
+
+        for (int i = 0, iMax = jaResults.length(); i < iMax; i++) {
+            JSONObject joTweet = jaResults.getJSONObject(i);
+
+            SimpleTuple outTuple = outPeer.createTuple();
+            outTuple.setValue("from_user", joTweet.getString("from_user"));
+            outTuple.setValue("from_user_id_str", joTweet.getString("from_user_id_str"));
+            outTuple.setValue("profile_image_url", joTweet.getString("profile_image_url"));
+            outTuple.setValue("created_at", joTweet.getString("created_at"));
+            outTuple.setValue("text", StringEscapeUtils.unescapeHtml(joTweet.getString("text")));
+
+            tweetsBuilder.addValue(outTuple.convert());
+        }
+
+        cc.pushDataComponentToOutput(OUT_META_TUPLE, outPeer.convert());
+        cc.pushDataComponentToOutput(OUT_TUPLES, tweetsBuilder.build());
+    }
+
+    @Override
+    public void disposeCallBack(ComponentContextProperties ccp) throws Exception {
+    }
+
+    //--------------------------------------------------------------------------------------------
+
+    @Override
+    protected void handleStreamInitiators() throws Exception {
+        if (!inputPortsWithInitiators.containsAll(Arrays.asList(new String[] { IN_TEXT })))
+            console.severe("Unbalanced stream delimiter received - the delimiters should arrive on all ports at the same time when FiringPolicy = ALL");
+
+        Object si = componentContext.getDataComponentFromInput(IN_TEXT);
+        componentContext.pushDataComponentToOutput(OUT_META_TUPLE, si);
+        componentContext.pushDataComponentToOutput(OUT_TUPLES, si);
+    }
+
+    @Override
+    protected void handleStreamTerminators() throws Exception {
+        if (!inputPortsWithTerminators.containsAll(Arrays.asList(new String[] { IN_TEXT })))
+            console.severe("Unbalanced stream delimiter received - the delimiters should arrive on all ports at the same time when FiringPolicy = ALL");
+
+        Object st = componentContext.getDataComponentFromInput(IN_TEXT);
+        componentContext.pushDataComponentToOutput(OUT_META_TUPLE, st);
+        componentContext.pushDataComponentToOutput(OUT_TUPLES, st);
+    }
+}
---
+++ src-tools/org/seasr/meandre/components/tools/tuples/TupleToHTML.java
@@ -0,0 +1,146 @@
+/**
+ * University of Illinois/NCSA
+ * Open Source License
+ *
+ * Copyright (c) 2008, Board of Trustees-University of Illinois.
+ * All rights reserved.
+ *
+ * Developed by:
+ *
+ * Automated Learning Group
+ * National Center for Supercomputing Applications
+ * http://www.seasr.org
+ *
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to
+ * deal with the Software without restriction, including without limitation the
+ * rights to use, copy, modify, merge, publish, distribute, sublicense, and/or
+ * sell copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ *  * Redistributions of source code must retain the above copyright notice,
+ *    this list of conditions and the following disclaimers.
+ *
+ *  * Redistributions in binary form must reproduce the above copyright notice,
+ *    this list of conditions and the following disclaimers in the
+ *    documentation and/or other materials provided with the distribution.
+ *
+ *  * Neither the names of Automated Learning Group, The National Center for
+ *    Supercomputing Applications, or University of Illinois, nor the names of
+ *    its contributors may be used to endorse or promote products derived from
+ *    this Software without specific prior written permission.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.  IN NO EVENT SHALL THE
+ * CONTRIBUTORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+ * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
+ * WITH THE SOFTWARE.
+ */
+
+package org.seasr.meandre.components.tools.tuples;
+
+import java.util.ArrayList;
+import java.util.LinkedHashMap;
+import java.util.List;
+import java.util.Map;
+
+import org.meandre.annotations.Component;
+import org.meandre.annotations.Component.Licenses;
+import org.meandre.annotations.Component.Mode;
+import org.meandre.annotations.ComponentInput;
+import org.meandre.annotations.ComponentProperty;
+import org.meandre.core.ComponentContext;
+import org.meandre.core.ComponentContextProperties;
+import org.seasr.datatypes.core.BasicDataTypes.Strings;
+import org.seasr.datatypes.core.BasicDataTypes.StringsArray;
+import org.seasr.datatypes.core.BasicDataTypesTools;
+import org.seasr.datatypes.core.Names;
+import org.seasr.meandre.components.vis.html.VelocityTemplateToHTML;
+import org.seasr.meandre.support.components.tuples.SimpleTuple;
+import org.seasr.meandre.support.components.tuples.SimpleTuplePeer;
+
+/**
+ * @author Boris Capitanu
+ */
+
+@Component(
+        creator = "Boris Capitanu",
+        description = "Cretes HTML for displaying a set of tuples in a table",
+        name = "Tuple To HTML",
+        tags = "tuple, html",
+        rights = Licenses.UofINCSA,
+        mode = Mode.webui,
+        baseURL = "meandre://seasr.org/components/foundry/",
+        dependency = { "velocity-1.6.2-dep.jar", "protobuf-java-2.2.0.jar" },
+        resources = { "TupleToHTML.vm" }
+)
+public class TupleToHTML extends VelocityTemplateToHTML {
+
+    //------------------------------ INPUTS ------------------------------------------------------
+
+    @ComponentInput(
+            name = Names.PORT_TUPLES,
+            description = "The tuples" +
+                "<br>TYPE: org.seasr.datatypes.BasicDataTypes.StringsArray"
+    )
+    protected static final String IN_TUPLES = Names.PORT_TUPLES;
+
+    @ComponentInput(
+            name = Names.PORT_META_TUPLE,
+            description = "The meta data for tuples" +
+                "<br>TYPE: org.seasr.datatypes.BasicDataTypes.Strings"
+    )
+    protected static final String IN_META_TUPLE = Names.PORT_META_TUPLE;
+
+    //------------------------------ PROPERTIES --------------------------------------------------
+
+    @ComponentProperty(
+            description = "The template name",
+            name = Names.PROP_TEMPLATE,
+            defaultValue = "org/seasr/meandre/components/tools/tuples/TupleToHTML.vm"
+    )
+    protected static final String PROP_TEMPLATE = Names.PROP_TEMPLATE;
+
+    //--------------------------------------------------------------------------------------------
+
+    @Override
+    public void initializeCallBack(ComponentContextProperties ccp) throws Exception {
+        super.initializeCallBack(ccp);
+
+        // Do component-specific initialization here
+    }
+
+    @Override
+    public void executeCallBack(ComponentContext cc) throws Exception {
+        Strings inMeta = (Strings) cc.getDataComponentFromInput(IN_META_TUPLE);
+        StringsArray input = (StringsArray) cc.getDataComponentFromInput(IN_TUPLES);
+
+        SimpleTuplePeer inPeer = new SimpleTuplePeer(inMeta);
+        Strings[] inTuples = BasicDataTypesTools.stringsArrayToJavaArray(input);
+
+        Map<String, List<String>> data = new LinkedHashMap<String, List<String>>(inPeer.size());
+        for (int i = 0, iMax = inPeer.size(); i < iMax; i++)
+            data.put(inPeer.getFieldNameForIndex(i), new ArrayList<String>());
+
+        for (Strings inTuple : inTuples) {
+            SimpleTuple tuple = inPeer.createTuple();
+            tuple.setValues(inTuple);
+
+            for (int i = 0, iMax = inPeer.size(); i < iMax; i++)
+                data.get(inPeer.getFieldNameForIndex(i)).add(tuple.getValue(i));
+        }
+
+        context.put("_tuplesCount", inTuples.length);
+        context.put("_tuples", data);
+
+        super.executeCallBack(cc);
+    }
+
+    @Override
+    public void disposeCallBack(ComponentContextProperties ccp) throws Exception {
+        super.disposeCallBack(ccp);
+    }
+}
